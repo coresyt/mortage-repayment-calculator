d8e505d61e601ed5ff72da747f7cc82f1000be92
coresyt <corejsyt2011@gmail.com>
2024-12-14 16:59:08 -0600

    chore(Root): Init project structure

---
+++ App.jsx
@@ -0,0 +1,41 @@
+export default function App() {
+  return (
+    <>
+      Mortgage Calculator <br />
+      Clear All
+      <br />
+      Mortgage Amount
+      <br />
+      Mortgage Term
+      <br />
+      Interest Rate
+      <br />
+      Mortgage Type
+      <br />
+      Repayment
+      <br />
+      Interest Only
+      <br />
+      Calculate Repayments
+      <br />
+      Results shown here
+      <br />
+      Complete the form and click “calculate repayments” to see what <br />
+      your monthly repayments would be.
+      <br />
+      {/* Empty results end */}
+      {/* Completed results start */}
+      Your results
+      <br />
+      Your results are shown below based on the information you provided. <br />
+      To adjust the results, edit the form and click “calculate repayments”
+      again.
+      <br />
+      Your monthly repayments
+      <br />
+      Total you&apos;ll repay over the term
+      <br />
+      {/* Completed results end */}
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./main.jsx", "./App.jsx", "./components/*.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          lime: "rgb(215, 218, 47)",
+          red: "rgb(215, 51, 40)",
+          white: "rgb(255, 255, 255)",
+          slate100: "rgb(227, 243, 253)",
+          slate300: "rgb(154, 190, 213)",
+          slate500: "rgb(107, 148, 168)",
+          slate700: "rgb(78, 110, 126)",
+          slate900: "rgb(18, 47, 63)"
+        }
+      },
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+}
