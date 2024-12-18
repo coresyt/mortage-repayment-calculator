2afbc74620679cd3c116cb1dd261eb1430839c19
coresyt <corejsyt2011@gmail.com>
2024-12-18 08:26:07 -0600

    feat(Root): Create functionality of the calculator of repayment
    BREAKING CHANGES:
    
    Change level of sementic's

--- App.jsx
+++ App.jsx
@@ -1,74 +1,180 @@
+import { useState } from "react"
+import { twMerge } from "tailwind-merge"
+import clsx from "clsx"
 import IconCalculator from "./assets/icon-calculator.svg"
 import IlustrationEmpty from "./assets/illustration-empty.svg"
 import "./styles/App.pcss"
 
 export default function App() {
+  const style = (...str) => twMerge(clsx(str))
+  const [amount, setAmount] = useState(0)
+  const [term, setTerm] = useState(0)
+  const [rate, setRate] = useState(0)
+  const [type, setType] = useState(0)
+  const [result, setResult] = useState({
+    resultWithInterest: {
+      total: "",
+      monthly: ""
+    },
+    isActive: false
+  })
+
+  /** @author miteshp98 */
+  const formatNumber = (number) =>
+    Intl.NumberFormat("en-UK", {
+      style: "currency",
+      currency: "GBP"
+    }).format(number)
+
+  /** @type {React.MouseEventHandler<HTMLButtonElement>} */
+  const handleCreateResult = (e) => {
+    e.preventDefault()
+    /** @author miteshp98 */
+    const monthlyInterest = rate / 100 / 12
+    const numberOfPayment = term * 12
+    const onePlusPowerN = Math.pow(1 + monthlyInterest, numberOfPayment)
+    const monthly =
+      (amount * (monthlyInterest * onePlusPowerN)) / (onePlusPowerN - 1)
+    const totalRepayment = monthly * numberOfPayment
+    const monthlyRepayment = formatNumber(monthly)
+    const totalRepaymentRounded = formatNumber(totalRepayment)
+
+    setResult({
+      isActive: true,
+      resultWithInterest: {
+        monthly: monthlyRepayment,
+        total: totalRepaymentRounded
+      }
+    })
+  }
+
+  /** @type {React.MouseEventHandler<HTMLAnchorElement>} */
+  const handleClearAllFields = (e) => {
+    e.preventDefault()
+    setAmount(0)
+    setTerm(0)
+    setType(0)
+    setResult({
+      resultWithInterest: { monthly: "", total: "" },
+      isActive: false
+    })
+  }
+
+  /**
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e
+   * @param {'rate' | 'amount' | 'term'} type
+   */
+  const handleChangeInput = (e, type) => {
+    const $el = e.currentTarget
+
+    type === "amount" && setAmount($el.value)
+    type === "term" && setTerm($el.value)
+    type === "rate" && setRate($el.value)
+  }
+
   return (
     <>
       <section className="sect-form">
         <div>
           <h1 className="sect-form__title">Mortgage Calculator</h1>
-          <a className="sect-form__clear" href="#">
+          <a
+            className="sect-form__clear"
+            href="#"
+            onClick={handleClearAllFields}
+          >
             Clear All
           </a>
         </div>
         <form className="sect-form__form">
-          <label className="sect-form__form__label lg:col-span-2">
+          <label className="sect-form__form__label x-invalid lg:col-span-2">
             Mortgage Amount
             <div className="sect-form__form__label__contain flex-row-reverse">
               <input
+                pattern="/^[+-]?(\.\d+|\d+\.|\d+|\d+\.?\d+|\d*\.?\d*[Ee][+-]?\d*)$/gm"
                 type="number"
                 min={0}
+                value={amount ?? 0}
+                onChange={(e) => handleChangeInput(e, "amount")}
                 className="sect-form__form__label__contain__input"
               />
               <div className="sect-form__form__label__contain__type">£</div>
             </div>
-          </label>
-          <label className="sect-form__form__label mr-5">
-            Mortgage Term
-            <div className="sect-form__form__label__contain">
-              <input
-                type="number"
-                className="sect-form__form__label__contain__input"
-              />
-              <div className="sect-form__form__label__contain__type">%</div>
-            </div>
+            <p className="amount hidden">This is field is required</p>
           </label>
           <label className="sect-form__form__label">
             Interest Rate
             <div className="sect-form__form__label__contain">
               <input
+                pattern="/^[+-]?(\.\d+|\d+\.|\d+|\d+\.?\d+|\d*\.?\d*[Ee][+-]?\d*)$/gm"
                 type="number"
+                min={0}
+                value={rate ?? 0}
+                onChange={(e) => handleChangeInput(e, "rate")}
                 className="sect-form__form__label__contain__input"
               />
               <div className="sect-form__form__label__contain__type sect-form__form__label__contain__type--rate">
                 years
               </div>
+            </div>{" "}
+            <p className="rate hidden">This is field is required</p>
+          </label>
+          <label className="sect-form__form__label lg:ml-5">
+            Mortgage Term
+            <div className="sect-form__form__label__contain">
+              <input
+                pattern="/^[+-]?(\.\d+|\d+\.|\d+|\d+\.?\d+|\d*\.?\d*[Ee][+-]?\d*)$/gm"
+                type="number"
+                min={0}
+                value={term ?? 0}
+                onChange={(e) => handleChangeInput(e, "term")}
+                className="sect-form__form__label__contain__input"
+              />
+              <div className="sect-form__form__label__contain__type">%</div>
             </div>
+            <p className="term hidden">This is field is required</p>
           </label>
           <label className="sect-form__form__label lg:col-span-2">
             Mortgage Type
-            <div className="active sect-form__form__label__contain sect-form__form__label__contain--type">
+            <div
+              className={style(
+                type === 1 ? "active" : "",
+                "sect-form__form__label__contain sect-form__form__label__contain--type"
+              )}
+              onClick={() => setType(type !== 1 ? 1 : 2)}
+            >
               <div className="sect-form__form__label__contain__select">
                 <span></span>
               </div>
               Repayment
             </div>
-            <div className="sect-form__form__label__contain sect-form__form__label__contain--type mb-4">
+            <div
+              className={style(
+                type === 2 ? "active" : "",
+                "sect-form__form__label__contain sect-form__form__label__contain--type"
+              )}
+              onClick={() => setType(type !== 2 ? 2 : 1)}
+            >
               <div className="sect-form__form__label__contain__select">
                 <span></span>
               </div>
               Interest Only
             </div>
           </label>
-          <button className="sect-form__form__button lg:col-span-2">
+          <button
+            className="sect-form__form__button lg:col-span-2"
+            onClick={handleCreateResult}
+          >
             <img src={IconCalculator} alt="Icon of a calculator" /> Calculate
             Repayments
           </button>
         </form>
       </section>
       {/* Empty results */}
-      <section className="sect-empty" hidden>
+      <section
+        className="sect-empty"
+        hidden={result.isActive !== true ? false : true}
+      >
         <div>
           <img
             className="sect-empty__image"
@@ -84,7 +190,10 @@ export default function App() {
         </div>
       </section>
       {/* Completed results */}
-      <section className="sect-result">
+      <section
+        className="sect-result"
+        hidden={result.isActive === true ? false : true}
+      >
         <div className="sect-result__contain">
           <h2 className="sect-result__contain__title">Your results</h2>
           <p className="sect-result__contain__desc">
@@ -95,12 +204,12 @@ export default function App() {
           <div className="sect-result__contain__result">
             <h3 className="sect-result__contain__result__monthly">
               Your monthly repayments
-              <span>£{(1797.74).toLocaleString()}</span>
+              <span>{result.resultWithInterest.monthly}</span>
             </h3>
             <hr className="sect-result__contain__result__line" />
             <h3 className="sect-result__contain__result__total">
               Total you&apos;ll repay over the term
-              <span>£{(539322.94).toLocaleString()}</span>
+              <span>{result.resultWithInterest.total}</span>
             </h3>
           </div>
         </div>
